3028c2b1aa4b575f3568bab3dd2a76584a9f9dad
unknown <kkolesnichenko@kkolesnichenko.exigengroup.com>
2015-10-19 16:05:45 +0300

    Fixing test failures

--- StudentsSources/src/test/java/com/students/ExceptionExamplesTests.java
+++ StudentsSources/src/test/java/com/students/ExceptionExamplesTests.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  */
 public class ExceptionExamplesTests {
 
-    @Test
+    @Test(expected =  RuntimeException.class)
     public void testTryCatchFinally(){
         try{
             throw new RuntimeException();
